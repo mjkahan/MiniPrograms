4c6fba8d5f44c6de8e330465d3e1efb9bbf86bd2
Michael Kahan <mjkahan23@gmail.com>
2019-08-24 20:24:51 -0500

    Initial commit

---
+++ MiniPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumGame.AskQuestion();
+        }
+    }
+}
---
+++ MiniPrograms/SumGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniPrograms
+{
+    class SumGame
+    {
+        public static void AskQuestion()
+        {
+            Top:
+            Random random = new Random();
+            int num = random.Next(10, 100);
+            int answer = (num / 10) + (num % 10);
+
+            Console.WriteLine("What is the sum of the two digits in the value " + num.ToString() + "?");
+
+            Start:
+            string response = Console.ReadLine();
+
+            if (answer.ToString() == response)
+            {
+                Console.WriteLine("You are correct!");
+                Console.Write("Play again? (Y/N) ");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    goto Top;
+                }
+                else
+                {
+                    Console.WriteLine("Okay, goodbye!");
+                    System.Threading.Thread.Sleep(2000);
+                    System.Environment.Exit(1);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that is incorrect. Guess again.");
+                goto Start;
+            }            
+        }
+    }
+}
